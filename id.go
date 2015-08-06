9dad99af30e6a03623e26c5a3ddce6db88d1dc0a
sumory <sumory.wu@gmail.com>
2015-08-06 23:18:13 +0800

    add id generator

---
+++ id.go
@@ -0,0 +1,29 @@
+package main
+
+
+import (
+	"crypto/rand"
+	"encoding/hex"
+	"io"
+	"fmt"
+)
+
+// uid returns a unique id. These ids consist of 128 bits from a
+// cryptographically strong pseudo-random generator and are like uuids, but
+// without the dashes and significant bits.
+//
+// See: http://en.wikipedia.org/wiki/UUID#Random_UUID_probability_of_duplicates
+func Uid() string {
+	id := make([]byte, 16)
+	_, err := io.ReadFull(rand.Reader, id)
+	if err != nil {
+		// This is probably an appropiate way to handle errors from our source
+		// for random bits.
+		panic(err)
+	}
+	return hex.EncodeToString(id)
+}
+
+func main() {
+	fmt.Println(Uid())
+}
